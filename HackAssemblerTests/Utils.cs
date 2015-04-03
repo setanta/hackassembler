58a97b1fe4428fa1641a8cddff349679828fbf16
Marcelo Lira <setanta@gmail.com>
2015-04-02 22:28:24 -0300

    Changes needed to have the project and tests running on Mac OS.

--- HackAssemblerTests/Utils.cs
+++ HackAssemblerTests/Utils.cs
@@ -7,11 +7,15 @@ namespace HackAssemblerTests
 {
     public static class Utils
     {
+        public static readonly string SamplesDir = "HackSamples";
+        public static readonly string SourcesDir = Path.Combine(SamplesDir, "Sources");
+        public static readonly string BinaryDir = Path.Combine(SamplesDir, "Binaries");
+
         public static void CompileAndCompare([CallerMemberName] string prog = "")
         {
-            var assembler = new Assembler(@"HackSamples\Sources\" + prog + ".asm");
+            var assembler = new Assembler(Path.Combine(SourcesDir, prog + ".asm"));
             assembler.Run();
-            var expected = File.ReadAllLines(@"HackSamples\Binaries\" + prog + ".hack");
+            var expected = File.ReadAllLines(Path.Combine(BinaryDir, prog + ".hack"));
             for (int i = 0; i < assembler.HackCode.Count; i++)
                 Assert.AreEqual(expected[i], assembler.HackCode[i]);
         }
